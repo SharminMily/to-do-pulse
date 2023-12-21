9ed07f7e688a6baf29e5a6024940e52ccc66ec03
SharminMily <sharminmily039@gmail.com>
2023-12-22 02:25:53 +0600

    google sign added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,9 @@
 import {
     Card,
     Input,
-    Checkbox,  
+    Checkbox,
     Typography,
+    Button,
 } from "@material-tailwind/react";
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -12,7 +13,7 @@ import { AuthContext } from "../../Provider/AuthProvider";
 const Login = () => {
 
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const { logIn  } = useContext(AuthContext);
+    const { logIn } = useContext(AuthContext);
     const [loginError, setLoginError] = useState('');
     // const location = useLocation();
     // const navigate = useNavigate();
@@ -23,18 +24,18 @@ const Login = () => {
         console.log(data);
         setLoginError('');
         logIn(data.email, data.password)
-        .then(result => {
-            const user = result.user;
-            console.log(user)
-        })
-        .catch(error => {
-            console.log(error.message)
-            setLoginError(error.message)
-        });
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.log(error.message)
+                setLoginError(error.message)
+            });
     }
 
     return (
-        <div className=" flex justify-center items-center my-10 mx-20">          
+        <div className=" flex justify-center items-center my-10 mx-20">
 
             <Card color="transparent" shadow={false}>
                 <Typography variant="h4" color="blue-gray">
@@ -43,18 +44,18 @@ const Login = () => {
                 <Typography color="gray" className="mt-1 font-normal">
                     Nice to meet you! Enter your details to Login.
                 </Typography>
-                
+
                 <form onSubmit={handleSubmit(handleLogin)} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="mb-1 flex flex-col gap-6">
-                       
+
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                             Your Email
                         </Typography>
                         <Input
-                         type="email"
-                         {...register("email", {
-                            required: "Email Address is required"
-                        })}
+                            type="email"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
                             size="lg"
                             placeholder="name@mail.com"
                             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
@@ -98,20 +99,30 @@ const Login = () => {
                         }
                         containerProps={{ className: "-ml-2.5" }}
                     />
-                  
+
                     <input className='bg-black py-2 rounded-md text-white w-full' value="Login" type="submit" />
 
                     <div>
                         {loginError && <p className='text-red-600'>{loginError}</p>}
                     </div>
 
-                    <Typography color="gray" className="mt-4 text-center font-normal">
-                        Already have an account?{" "}
-                        <a href="register" className="font-medium text-blue-900">
-                            register
-                        </a>
-                    </Typography>
                 </form>
+                <Button
+                    size="lg"
+                    variant="outlined"
+                    color=""
+                    className="flex font-normal justify-center normal-case bg-black p-2 text-white text-center items-center gap-3"
+                >
+                    <img src="https://docs.material-tailwind.com/icons/google.svg" alt="metamask" className="h-6 w-6" />
+                    Continue with Google
+                </Button>
+
+                <Typography color="gray" className="mt-4 text-center font-normal">
+                    Already have an account?{" "}
+                    <a href="register" className="text-black font-semibold">
+                        Register
+                    </a>
+                </Typography>
             </Card>
 
         </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 import {
+    Button,
     Card,
-    Input,   
+    Input,
     Typography,
 } from "@material-tailwind/react";
 import { useContext, useState } from "react";
@@ -14,19 +15,19 @@ const Register = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const { createUser } = useContext(AuthContext);
     // const [loginError, setLoginError] = useState('');
-    const location = useLocation();  
+    const location = useLocation();
 
     const from = location.state?.from?.pathname || '/';
 
     const handleSignUp = (data) => {
         console.log(data);
         // setLoginError('');
-        createUser( data.email, data.password)
-        .then(result => {
-            const user = result.user;
-            console.log(user)
-        })
-        .catch(error => console.log(error));
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => console.log(error));
 
         // signIn(data.email, data.password)
         //     .then(result => {
@@ -41,79 +42,89 @@ const Register = () => {
     }
 
     return (
-        <div className=" flex justify-center items-center my-10 mx-20">          
+        <div className=" flex justify-center items-center my-10 mx-20">
 
-        <Card color="transparent" shadow={false}>
-            <Typography variant="h4" color="blue-gray">
-                Register
-            </Typography>
-            <Typography color="gray" className="mt-1 font-normal">
-                Nice to meet you! Enter your details to register.
-            </Typography>
-            
-            <form onSubmit={handleSubmit(handleSignUp)} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                <div className="mb-1 flex flex-col gap-6">
-                   {/*  */}
-                    <div  color="blue-gray" className="-mb-3 font-bold">
-                        Your Name
+            <Card color="transparent" shadow={false}>
+                <Typography variant="h4" color="blue-gray">
+                    Register
+                </Typography>
+                <Typography color="gray" className="mt-1 font-normal">
+                    Nice to meet you! Enter your details to register.
+                </Typography>
+
+                <form onSubmit={handleSubmit(handleSignUp)} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                    <div className="mb-1 flex flex-col gap-6">
+                        {/*  */}
+                        <div color="blue-gray" className="-mb-3 font-bold">
+                            Your Name
+                        </div>
+                        <Input
+                            type="name"
+                            {...register("name", {
+                                required: "name"
+                            })}
+                            size="lg"
+                            placeholder="name@mail.com"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <div color="blue-gray" className="-mb-3 font-bold">
+                            Your Email
+                        </div>
+                        <Input
+                            type="email"
+                            {...register("email", {
+                                required: "Email Address is required"
+                            })}
+                            size="lg"
+                            placeholder="name@mail.com"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
+                        <div color="blue-gray" className="-mb-3 font-bold">
+                            Password
+                        </div>
+                        <Input
+                            type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            size="lg"
+                            placeholder="********"
+                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            labelProps={{
+                                className: "before:content-none after:content-none",
+                            }}
+                        />
                     </div>
-                    <Input
-                     type="name"
-                     {...register("name", {
-                        required: "name"
-                    })}
-                        size="lg"
-                        placeholder="name@mail.com"
-                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                        labelProps={{
-                            className: "before:content-none after:content-none",
-                        }}
-                    />
-                    <div  color="blue-gray" className="-mb-3 font-bold">
-                        Your Email
-                    </div>
-                    <Input
-                     type="email"
-                     {...register("email", {
-                        required: "Email Address is required"
-                    })}
-                        size="lg"
-                        placeholder="name@mail.com"
-                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                        labelProps={{
-                            className: "before:content-none after:content-none",
-                        }}
-                    />
-                    <div  color="blue-gray" className="-mb-3 font-bold">
-                        Password
-                    </div>
-                    <Input
-                        type="password"
-                        {...register("password", {
-                            required: "Password is required",
-                            minLength: { value: 6, message: 'Password must be 6 characters or longer' }
-                        })}
-                        size="lg"
-                        placeholder="********"
-                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                        labelProps={{
-                            className: "before:content-none after:content-none",
-                        }}
-                    />
-                </div>              
-              
-                <input className='bg-black mt-4 py-2 rounded-md text-white w-full' value="Register" type="submit" />
+
+                    <input className='bg-black mt-4 py-2 rounded-md text-white w-full' value="Register" type="submit" />
+                </form>
+                <Button
+                    size="lg"
+                    variant="outlined"
+                    color=""
+                    className="flex font-normal justify-center normal-case bg-black p-2 text-white text-center items-center gap-3"
+                >
+                    <img src="https://docs.material-tailwind.com/icons/google.svg" alt="metamask" className="h-6 w-6" />
+                    Continue with Google
+                </Button>
 
                 <Typography color="gray" className="mt-4 text-center font-normal">
                     Create an account?{" "}
-                    <a href="login" className="font-medium text-gray-900">
-                       login
+                    <a href="login" className="text-black font-semibold">
+                        Login
                     </a>
                 </Typography>
-            </form>
-        </Card>
 
-    </div>
+            </Card>
+
+        </div>
     );
 };
 
